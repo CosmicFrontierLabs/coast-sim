ae4b2187281fc5302f4c04686749cb5ad772fc7e
Jamie Kennea <jak574@gmail.com>
2025-11-24 16:16:47 -0500

    feat: add data management visualizer

--- conops/visualization/__init__.py
+++ conops/visualization/__init__.py
@@ -1,5 +1,10 @@
 """Visualization utilities for CONOPS simulations."""
 
+from .data_management import plot_data_management_telemetry
 from .ditl_timeline import annotate_slew_distances, plot_ditl_timeline
 
-__all__ = ["plot_ditl_timeline", "annotate_slew_distances"]
+__all__ = [
+    "plot_ditl_timeline",
+    "annotate_slew_distances",
+    "plot_data_management_telemetry",
+]
