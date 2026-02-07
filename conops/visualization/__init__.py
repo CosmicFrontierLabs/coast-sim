d7f21258e5a466281c51b1225920faafe103f762
Jamie Kennea <jak51@psu.edu>
2026-02-07 08:43:48 -0500

    fix: add eclipse to ditl plot, add plotly version

--- conops/visualization/__init__.py
+++ conops/visualization/__init__.py
@@ -2,7 +2,7 @@
 
 from .acs_mode_analysis import plot_acs_mode_distribution
 from .data_management import plot_data_management_telemetry
-from .ditl_telemetry import plot_ditl_telemetry
+from .ditl_telemetry import plot_ditl_telemetry, plot_ditl_telemetry_plotly
 from .ditl_timeline import annotate_slew_distances, plot_ditl_timeline
 from .sky_pointing import (
     plot_sky_pointing,
@@ -19,4 +19,5 @@ __all__ = [
     "plot_sky_pointing",
     "save_sky_pointing_frames",
     "save_sky_pointing_movie",
+    "plot_ditl_telemetry_plotly",
 ]
