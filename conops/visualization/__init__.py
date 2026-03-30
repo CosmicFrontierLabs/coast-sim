0785de83ea0c3f8b7f628a224dbccbbcd90baf3f
Jamie Kennea <jak51@psu.edu>
2026-03-30 12:10:19 -0400

    fix: add plotly version of plot_sky_pointing

--- conops/visualization/__init__.py
+++ conops/visualization/__init__.py
@@ -12,7 +12,7 @@ from .fault_management import (
     plot_fault_management_timeline,
     plot_fault_management_timeline_plotly,
 )
-from .globe_pointing import plot_sky_pointing_globe
+from .globe_pointing import plot_sky_pointing_globe, plot_sky_pointing_plotly
 from .sky_pointing import (
     plot_sky_pointing,
     save_sky_pointing_frames,
@@ -29,6 +29,7 @@ __all__ = [
     "plot_fault_management_timeline_plotly",
     "plot_sky_pointing",
     "plot_sky_pointing_globe",
+    "plot_sky_pointing_plotly",
     "save_sky_pointing_frames",
     "save_sky_pointing_movie",
     "plot_ditl_telemetry_plotly",
